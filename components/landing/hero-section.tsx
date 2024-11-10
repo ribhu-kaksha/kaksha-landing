25a60da0c8033173ce9e8afe418435c1d8a27ba3
ribhu-kaksha <ribhu@kaksha.ai>
2024-11-10 15:39:55 +0530

    basic fix

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -17,21 +17,20 @@ export default function HeroSection() {
     >
       <div className="backdrop-filter-[12px] inline-flex h-7 items-center justify-between rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white dark:text-black transition-all ease-in hover:cursor-pointer hover:bg-white/20 group gap-1 translate-y-[-1rem] animate-fade-in opacity-0">
         <TextShimmer className="inline-flex items-center justify-center">
-          <span>✨ Introducing Magic UI Template</span>{" "}
+          <span>✨ Introducing an ai-powered classroom</span>{" "}
           <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
         </TextShimmer>
       </div>
       <h1 className="bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
-        Magic UI is the new way
-        <br className="hidden md:block" /> to build landing pages.
+        kaksha.ai is the new way
+        <br className="hidden md:block" /> to teach online.
       </h1>
       <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
-        Beautifully designed, animated components and templates built with
-        <br className="hidden md:block" /> Tailwind CSS, React, and Framer
-        Motion.
+        An intuitive platform for students to take courses uploaded by their teachers
+        <br className="hidden md:block" /> with real-time help from our AI like a one-on-one tutor.
       </p>
       <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]">
-        <span>Get Started for free </span>
+        <span>Start Teaching </span>
         <ArrowRightIcon className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
       </Button>
       <div
