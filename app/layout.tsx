9b2d6baeeeb4f6dcc749830db27ab3c5396515c6
ribhu-kaksha <ribhu@kaksha.ai>
2024-11-10 21:39:44 +0530

    Updated wrong font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
-import { Open_Sans as FontSans } from "next/font/google";
+import { Alata as FontSans } from "next/font/google";
 import "./globals.css";
 
 const fontSans = FontSans({
